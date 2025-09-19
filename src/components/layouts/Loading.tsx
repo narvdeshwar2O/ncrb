3c083d3f5b3f15af2f520b037bdfec7b10cb64b9
Narvdeshwar <narvdeshwar@xenelsoft.co.in>
2025-09-19 11:40:08 +0530

    fixed loding

--- src/components/layouts/Loading.tsx
+++ src/components/layouts/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="h-screen w-screen flex flex-col justify-center bg-card">
+    <div className="h-screen w-screen flex flex-col justify-center items-center bg-card">
       <img src={Logo} alt="Logo" className="size-10"/>
       <p>Nafis Dashboard Loading...</p>
     </div>
